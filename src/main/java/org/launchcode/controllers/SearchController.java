a11d36b630361cb5cd2aea804e09f66d808fefa8
thomaslitton <thomas.litton@centurylink.com>
2017-03-23 22:03:13 -0500

    Lets start with the simplest thing possible.  In this case i'm just implementing the method at the first @TODO, so that it gets called when the search button is pressed.
    I"m just printing out that the method is called, and going back to the same page for now.
    
    At this point the search button doesn't result in an error, but doesn't exactly show the right information.  In fact, it shows less then it did before!

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -16,12 +16,51 @@ import java.util.HashMap;
 @RequestMapping("search")
 public class SearchController {
 
+    /**
+     * This is called from the link in the fragments.html:
+     * `<a th:href="@{/search}">Search</a>`
+     *
+     * as mentioned below, `search` is defined above by the class,
+     * which means `/search` goes to here.
+     *
+     * @param model
+     * @return
+     */
     @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", ListController.columnChoices);
+
+        // I'm returning the same thing as above.
+        // the value here gets `.html` added to it to
+        // find the html page to use.  So `search` becomes
+        // `search.html` and therefor the `search.html`
+        // file gets used.
         return "search";
     }
 
     // TODO #1 - Create handler to process search request and display results
 
+    /**
+     * I know this should be `results` because of the path used in the
+     * form in the search.html page:
+     *
+     * `<form th:action="@{/search/results}">`
+     *
+     * `search` part is defined above, so we only need the last part here
+     *
+     *  Feel free to experiment by changing the form action in the search.html
+     *  page and the value here.  See what works and what doesn't.
+     */
+    @RequestMapping(value = "results")
+    public String searchResults(Model model) {
+
+        System.out.println("I'm in search results!!!");
+
+        // I'm returning the same thing as above.
+        // the value here gets `.html` added to it to
+        // find the html page to use.  So `search` becomes
+        // `search.html` and therefor the `search.html`
+        // file gets used.
+        return "search";
+    }
 }
